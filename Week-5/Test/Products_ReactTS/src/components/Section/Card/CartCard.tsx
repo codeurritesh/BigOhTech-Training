26bf867cbd55850c4624f0998e258c6ae6efd5b0
codeurritesh <riteshponty@gmail.com>
2024-05-12 16:48:03 +0530

    updates

--- Week-5/Test/Products_ReactTS/src/components/Section/Card/CartCard.tsx
+++ Week-5/Test/Products_ReactTS/src/components/Section/Card/CartCard.tsx
@@ -2,11 +2,13 @@ import { Product } from "../../../datatypes/Product";
 import "./CartCard.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { addToCart, removeFromCart } from "../../../ReduxManagement/Actions/Cart-Actions/Cart-Actions";
+import {
+  addToCart,
+  removeFromCart,
+} from "../../../ReduxManagement/Actions/Cart-Actions/Cart-Actions";
 import { colors } from "@mui/material";
 
-
-const CartCard = (props: { product: Product}) => {
+const CartCard = (props: { product: Product }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const addToCartHandler = (product: Product) => {
@@ -18,29 +20,38 @@ const CartCard = (props: { product: Product}) => {
 
   return (
     <div className="cart-card">
-        <div className="cart-product">
-            <div className="cart-product-info">
-            <img src={props.product.thumbnail} alt="" />
-            <div className="product-infos">
-                <b>{props.product.title}</b>
-                <p>{props.product.description.substring(0,50)}...</p>
-            </div>
-            </div>
-            <h4 className="mrp">$ <span style={{textDecoration:"line-through"}}>{props.product?.price
-                ? Math.ceil(
-                    props.product?.price +
-                      (props.product?.discountPercentage / 100) *
-                      props.product?.price
-                  )
-                : "$"}</span><small style={{color:"green",marginLeft:"5px"}}>{props.product.discountPercentage}% Off</small></h4>
-            <h4>$ {props.product.price}</h4>
-            <i onClick={(event) => {
-                removeFromCartHandler(props.product);
-                event.stopPropagation();
-              }} className="fa-regular fa-trash-can"></i>
+      <div className="cart-product">
+        <div className="cart-product-info">
+          <img src={props.product.thumbnail} alt="" />
+          <div className="product-infos">
+            <b>{props.product.title}</b>
+            <p>{props?.product?.description?.substring(0, 50)}...</p>
+          </div>
         </div>
-
-
+        <h4 className="mrp">
+          ${" "}
+          <span style={{ textDecoration: "line-through" }}>
+            {props.product?.price
+              ? Math.ceil(
+                  props.product?.price +
+                    (props.product?.discountPercentage / 100) *
+                      props.product?.price
+                )
+              : "$"}
+          </span>
+          <small style={{ color: "green", marginLeft: "5px" }}>
+            {props.product.discountPercentage}% Off
+          </small>
+        </h4>
+        <h4>$ {props.product.price}</h4>
+        <i
+          onClick={(event) => {
+            removeFromCartHandler(props.product);
+            event.stopPropagation();
+          }}
+          className="fa-regular fa-trash-can"
+        ></i>
+      </div>
     </div>
   );
 };
